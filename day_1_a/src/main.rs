b86448758f20291af62ede7716d731cfd3a8ac69
katiras <thanoskat@gmx.com>
2025-12-01 21:34:15 +0200

    day 1 a

---
+++ day_1_a/src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+enum Rotation {
+    Left(u32),
+    Right(u32),
+}
+
+fn main() {
+    let file = std::fs::File::open("./input.txt");
+    let lines = io::BufReader::new(file.unwrap()).lines();
+
+    let mut rotations = Vec::<Rotation>::new();
+
+    for line in lines.map_while(Result::ok) {
+        let mut chars = line.chars();
+
+        let rotation = match chars.next().unwrap() {
+            'L' => Rotation::Left(chars.as_str().parse::<u32>().unwrap()),
+            'R' => Rotation::Right(chars.as_str().parse::<u32>().unwrap()),
+            _ => {
+                panic!("Inalid input");
+            }
+        };
+        
+        rotations.push(rotation);
+    }
+    
+    let mut current_pos = 50;
+    let mut zero_count = 0;
+    
+    for rotation in rotations {
+        let pos_diff = match rotation {
+            Rotation::Left(n) => n as i32 * -1,
+            Rotation::Right(n) => n as i32,
+        };
+        
+        current_pos = (current_pos + pos_diff + 100) % 100;
+        
+        if current_pos == 0 {
+            zero_count += 1;
+        }
+    }
+    
+    println!("{}", zero_count);
+}
